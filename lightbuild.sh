e6c2eabe243777a9ebdd8b95d0a940f39baa0f1c
sookyboo <sookyboo@gmail.com>
2026-02-09 16:54:21 +0000

    Add windows scripts.

--- lightbuild.sh
+++ lightbuild.sh
@@ -22,8 +22,13 @@ docker cp noxdecomp_tmp-light:/build/nox-decomp/build/src/out ../noxd.armhf
 # docker cp noxdecomp_tmp-light:/opt/ffmpeg-armhf/lib/. ../ffmpeg.armhf/ && \
 docker rm noxdecomp_tmp-light
 
-docker buildx build --load --platform=linux/amd64 --progress=plain -f Dockerfile.x86-light -t noxdecomp-build-x86-light . &&  \
-docker create --name noxdecomp_tmp-x86-light noxdecomp-build-x86-light && \
-docker cp noxdecomp_tmp-x86-light:/build/nox-decomp/build/src/out ../noxd.i386
-#docker cp noxdecomp_tmp-x86-light:/opt/ffmpeg-i386/lib/. ../ffmpeg.i386/ && \
-docker rm noxdecomp_tmp-x86-light
+docker context use default
+docker load -i ../noxdecomp-build-win-x86.tar
+docker rm noxdecomp_tmp-win-x86-light
+docker buildx build --load --platform=linux/amd64 --progress=plain -f Dockerfile.winx86-light -t noxdecomp-build-win-x86-light . &&  \
+docker create --name noxdecomp_tmp-win-x86-light noxdecomp-build-win-x86-light && \
+rm -rf ../noxd.win32 && \
+docker cp noxdecomp_tmp-win-x86-light:/build/nox-decomp/build/src/noxd.win32 ../noxd.win32 && \
+# optional rename if you want a nicer exe name locally
+( test -f ../noxd.win32/out.exe && mv -f ../noxd.win32/out.exe ../noxd.win32/noxd.winx86.exe || true ) && \
+docker rm noxdecomp_tmp-win-x86-light
