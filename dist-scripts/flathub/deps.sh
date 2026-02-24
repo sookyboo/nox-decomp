e653371ac3447ea29c7512815f66c67b336656b8
sookyboo <sookyboo@gmail.com>
2026-02-24 22:58:58 +0000

    Add fixes for flathub scripts.

--- dist-scripts/flathub/deps.sh
+++ dist-scripts/flathub/deps.sh
@@ -1,2 +1,6 @@
 flatpak install -y flathub org.flatpak.Builder
 flatpak install -y flathub org.freedesktop.Platform//24.08 org.freedesktop.Sdk//24.08
+flatpak install -y flathub \
+  org.freedesktop.Sdk//24.08 \
+  org.freedesktop.Sdk.Compat.i386//24.08 \
+  org.freedesktop.Sdk.Extension.toolchain-i386//24.08
