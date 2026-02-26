57cd1847cfc1ac6efbbbdb949e22371b1e60b197
sookyboo <sookyboo@gmail.com>
2026-02-26 12:04:00 +0000

    Update flatpak deps.sh.

--- dist-scripts/flathub/deps.sh
+++ dist-scripts/flathub/deps.sh
@@ -1,6 +1,6 @@
 flatpak install -y flathub org.flatpak.Builder
-flatpak install -y flathub org.freedesktop.Platform//24.08 org.freedesktop.Sdk//24.08
+flatpak install -y flathub org.freedesktop.Platform//25.08 org.freedesktop.Sdk//25.08
 flatpak install -y flathub \
-  org.freedesktop.Sdk//24.08 \
-  org.freedesktop.Sdk.Compat.i386//24.08 \
-  org.freedesktop.Sdk.Extension.toolchain-i386//24.08
+  org.freedesktop.Sdk//25.08 \
+  org.freedesktop.Sdk.Compat.i386//25.08 \
+  org.freedesktop.Sdk.Extension.toolchain-i386//25.08
