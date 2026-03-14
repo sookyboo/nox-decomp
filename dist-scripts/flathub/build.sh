dd78565e159ac5f988aba1ef2787e25e1617304a
sookyboo <sookyboo@gmail.com>
2026-03-14 20:13:32 +0000

    Fix flatpak script.

--- dist-scripts/flathub/build.sh
+++ dist-scripts/flathub/build.sh
@@ -1,9 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 APP_ID=io.github.sookyboo.nox-decomp
 MANIFEST=io.github.sookyboo.nox-decomp.yml
 
 rm -rf build-dir repo
+ostree --repo=repo init --mode=archive-z2
+ostree --repo=repo config set core.min-free-space-percent 0
+
 flatpak-builder --force-clean --repo=repo build-dir "$MANIFEST"
 flatpak build-update-repo repo
-
-# Optional: create a single-file bundle
-flatpak build-bundle repo ${APP_ID}.flatpak "$APP_ID"
+flatpak build-bundle repo "${APP_ID}.flatpak" "$APP_ID"
