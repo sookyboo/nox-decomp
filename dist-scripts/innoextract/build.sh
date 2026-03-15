cedd48372bec0305d9e801d619683e7cef114436
sookyboo <sookyboo@gmail.com>
2026-03-15 23:05:22 +0000

    Fix innoextract build

--- dist-scripts/innoextract/build.sh
+++ dist-scripts/innoextract/build.sh
@@ -1,20 +1,5 @@
 docker buildx build \
-  --platform=linux/amd64 \
-  --target linux-amd64 \
-  -f Dockerfile.innoextract \
-  -o type=local,dest=./out/linux-x86_64 \
-  .
-
-docker buildx build \
-  --platform=linux/arm64 \
-  --target linux-arm64 \
-  -f Dockerfile.innoextract \
-  -o type=local,dest=./out/linux-aarch64 \
-  .
-
-docker buildx build \
-  --platform=linux/amd64 \
-  --target windows-amd64 \
-  -f Dockerfile.innoextract \
-  -o type=local,dest=./out/windows-x86_64 \
+  --target artifacts \
+  -f Dockerfile \
+  -o type=local,dest=./out \
   .
